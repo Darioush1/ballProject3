05e535730c0230573cc72162d183f5768cfbad4d
Erik H <erikjh_24@yahoo.com>
2019-10-20 20:50:28 -0700

    Added AutoCompleteTest component with styling, updated news link to be for the NBA, added components for all NBA players and all NBA teams to use on a temporary basis. Will need to call this data from the API once it has been implemented.

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './homepage.css';
 import Stats from '../stats/stats';
 import News from '../news/news';
+import AutoCompleteText from "../components/AutoCompleteText/AutoCompleteText";
+import Players from "../components/Players/Players";
+import Teams from "../components/Teams/Teams";
 
 class HomePage extends React.Component {
 
@@ -13,10 +16,10 @@ class HomePage extends React.Component {
         this.updatePlayer2 = this.updatePlayer2.bind(this);
         this.updateTeam1 = this.updateTeam1.bind(this);
         this.updateTeam2 = this.updateTeam2.bind(this);
-        this.state = { 
-            player1: '', 
-            player2: '', 
-            team1: '', 
+        this.state = {
+            player1: '',
+            player2: '',
+            team1: '',
             team2: '',
             submitted: false,
             data: {}
@@ -30,10 +33,10 @@ class HomePage extends React.Component {
 
     submitTeams(e) {
         e.preventDefault();
-        fetch("http://site.api.espn.com/apis/site/v2/sports/football/college-football/news")
+        fetch("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/news")
             .then(res => res.json())
             .then(data => {
-                this.setState({data: data, submitted: true});
+                this.setState({ data: data, submitted: true });
             });
         // console.log(this.state.team1, this.state.team2);
     }
@@ -65,12 +68,13 @@ class HomePage extends React.Component {
                                 <h5 className="card-title">ENTER PLAYER NAMES</h5>
                                 <form onSubmit={this.submitPlayers}>
                                     <div className="form-group">
-                                        <label htmlFor="playerName1">PLAYER NAME 1</label>
-                                        <input type="text" value={this.state.player1} onChange={this.updatePlayer1} className="form-control" id="playerName1" aria-describedby="playerName1" placeholder="Enter Player Name 1" />
+                                        <label htmlFor="playerName1">PLAYER NAME 1</label>                      <AutoCompleteText items={Players}  />
+                                        {/* <input type="text" value={this.state.player1} onChange={this.updatePlayer1} className="form-control" id="playerName1" aria-describedby="playerName1" placeholder="Enter Player Name 1" /> */}
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="playerName2">PLAYER NAME 2</label>
-                                        <input type="text" value={this.state.player2} onChange={this.updatePlayer2} className="form-control" id="playerName2" aria-describedby="playerName2" placeholder="Enter Player Name 2" />
+                                        <AutoCompleteText items={Players}  />
+                                        {/* <input type="text" value={this.state.player2} onChange={this.updatePlayer2} className="form-control" id="playerName2" aria-describedby="playerName2" placeholder="Enter Player Name 2" /> */}
                                     </div>
                                     <button type="submit" className="btn btn-primary">SUBMIT</button>
                                 </form>
@@ -85,11 +89,13 @@ class HomePage extends React.Component {
                                 <form onSubmit={this.submitTeams}>
                                     <div className="form-group">
                                         <label htmlFor="teamName1">TEAM NAME 1</label>
-                                        <input type="text" defaultValue={this.state.team1} onChange={() => {}} onBlur={this.updateTeam1} className="form-control" id="teamName1" aria-describedby="teamName1" placeholder="Enter Team Name 1" />
+                                        <AutoCompleteText items={Teams} />
+                                        {/* <input type="text" defaultValue={this.state.team1} onChange={() => { }} onBlur={this.updateTeam1} className="form-control" id="teamName1" aria-describedby="teamName1" placeholder="Enter Team Name 1" /> */}
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="teamName2">TEAM NAME 2</label>
-                                        <input type="text" defaultValue={this.state.team2} onChange={() => {}} onBlur={this.updateTeam2} className="form-control" id="teamName2" aria-describedby="teamName2" placeholder="Enter Team Name 2" />
+                                        <AutoCompleteText items={Teams} />
+                                        {/* <input type="text" defaultValue={this.state.team2} onChange={() => { }} onBlur={this.updateTeam2} className="form-control" id="teamName2" aria-describedby="teamName2" placeholder="Enter Team Name 2" /> */}
                                     </div>
                                     <button type="submit" className="btn btn-primary">SUBMIT</button>
                                 </form>
@@ -100,36 +106,36 @@ class HomePage extends React.Component {
                 <div className="row">
                     {this.state.submitted &&
                         <div className="card w-100 ml-3 mr-3 mt-4">
-                        <div className="col">
-                            <div className="card-body">
-                                <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                                    <li className="nav-item">
-                                        <a className="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Stats</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">News</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Photos</a>
-                                    </li>
-                                </ul>
-                                <div className="tab-content" id="pills-tabContent" style={{minHeight: '400px'}}>
-                                    <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" style={{minHeight:'400px'}}>
-                                        <Stats
-                                            team1={this.state.team1}
-                                            team2={this.state.team2}
-                                        />
+                            <div className="col">
+                                <div className="card-body">
+                                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                                        <li className="nav-item">
+                                            <a className="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Stats</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">News</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Photos</a>
+                                        </li>
+                                    </ul>
+                                    <div className="tab-content" id="pills-tabContent" style={{ minHeight: '400px' }}>
+                                        <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" style={{ minHeight: '400px' }}>
+                                            <Stats
+                                                team1={this.state.team1}
+                                                team2={this.state.team2}
+                                            />
+                                        </div>
+                                        <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" style={{ height: '500px' }}>
+                                            <News
+                                                data={this.state.data}
+                                            />
+                                        </div>
+                                        <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
                                     </div>
-                                    <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" style={{height: '500px'}}>
-                                        <News
-                                            data={this.state.data}
-                                        />
-                                    </div>
-                                    <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                     }
                 </div>
             </div>
