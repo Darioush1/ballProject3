d3466beb4019cb1dcf31dd0280a4e3cca2679bcf
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-10-26 19:00:26 -0700

    api calls database

--- api/app.js
+++ api/app.js
@@ -3,11 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/Nbastat",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
