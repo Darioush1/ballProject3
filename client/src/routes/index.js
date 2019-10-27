1cce002cc069c18d8be7b1f5bf85c946b9db86f3
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-10-27 02:32:12 -0700

    added routes to front end to call data directly

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -5,6 +5,20 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
+const player1 = require("./routes/player1");
+const player2 = require("./routes/plaer2");
+const team1 = require("./routes/team1");
+const team2 = require("./routes/team2");
+
+router.use("./routes/player1", player1);
+router.use("./routes/player2", player2);
+router.use("./routes/team1", team1);
+router.use("./routes/team2", team2);
+
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+  
 
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
