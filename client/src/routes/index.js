113e1a61d2ddc9b8c4c1f7d137986e5af36f888a
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-10-27 02:17:13 -0700

    more stuff

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const player1 = require("./routes/playerr1");
+const player2 = require("./routes/plaer2");
+const team1 = require("./routes/team1");
+const team2 = require("./routes/team2");
 
 
 router.use("/api", apiRoutes);
