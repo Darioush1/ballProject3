ee528e29832cafb544c805e0fb24d554b8be2de5
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-11-02 00:09:38 -0700

    the query call is being fixed

--- client/src/homepage/homepage.js
+++ client/src/homepage/homepage.js
@@ -49,129 +49,129 @@ class HomePage extends React.Component {
 
     
 
-// //api post route for player info
-//     addPlayer = event => {
+//api post route for player info
+    addPlayer = event => {
         
-//         const userInput = "LeBron James";
-//         let players = this.state.data;
-//         const choice = players.filter(players => players.name === userInput);
-//         const choice1 = choice[0];
-//         console.log(choice1);
-//         api.savePlayer1({
-//             name: choice1.name
-//          }).then(res => choice1)
-//     };
+        const userInput = "LeBron James";
+        let players = this.state.data;
+        const choice = players.filter(players => players.name === userInput);
+        const choice1 = choice[0];
+        console.log(choice1);
+        api.savePlayer1({
+            name: choice1.name
+         }).then(res => choice1)
+    };
 
-//     constructor(props) {
-//         super(props);
-//         this.submitPlayers = this.submitPlayers.bind(this);
-//         this.submitTeams = this.submitTeams.bind(this);
-//         this.updatePlayer1 = this.updatePlayer1.bind(this);
-//         this.updatePlayer2 = this.updatePlayer2.bind(this);
-//         this.updateTeam1 = this.updateTeam1.bind(this);
-//         this.updateTeam2 = this.updateTeam2.bind(this);
-//         this.state = {
-//             statsData: {}
-//         }
-//         // this.state = {
-//         //     player1: '',
-//         //     player2: '',
-//         //     team1: '',
-//         //     team2: '',
-//         //     submitted: false,
-//         //     data: {}
-//     };
+    constructor(props) {
+        super(props);
+        this.submitPlayers = this.submitPlayers.bind(this);
+        this.submitTeams = this.submitTeams.bind(this);
+        this.updatePlayer1 = this.updatePlayer1.bind(this);
+        this.updatePlayer2 = this.updatePlayer2.bind(this);
+        this.updateTeam1 = this.updateTeam1.bind(this);
+        this.updateTeam2 = this.updateTeam2.bind(this);
+        this.state = {
+            statsData: {}
+        }
+        // this.state = {
+        //     player1: '',
+        //     player2: '',
+        //     team1: '',
+        //     team2: '',
+        //     submitted: false,
+        //     data: {}
+    };
 
-//     componentWillMount() {
-//         this.getStatsData();
-//     }
+    componentWillMount() {
+        this.getStatsData();
+    }
 
-//     getStatsData() {
-//         // Ajax call goes here, fill the state with the data that comes in
-//         this.setState({
-//             statsData: {
-//                 // The data below is hard coded. Need to change it so it dynamically pulls data depending on which teams the user selects
-//                 labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9',
-//                     '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
-//                     '20', '22', '22', '23', '24', '25', '26', '27', '28', '29',
-//                     '30', '33', '32', '33', '34', '35', '36', '37', '38', '39',
-//                     '40', '44', '42', '43', '44', '45', '46', '47', '48', '49',
-//                     '50', '55', '52', '53', '54', '55', '56', '57', '58', '59',
-//                     '60', '66', '62', '63', '64', '65', '66', '67', '68', '69',
-//                     '70', '77', '72', '73', '74', '75', '76', '77', '78', '79',
-//                     '80', '81', '82'],
-//                 datasets: [
-//                     {
-//                         label: 'Houston Rockets Pts Per Game',
-//                         data: [112, 124, 112, 89, 113, 85, 119, 96, 98,
-//                             80, 89, 115, 109, 107, 132, 126, 111, 108, 131,
-//                             108, 136, 121, 91, 91, 104, 111, 126, 105, 102,
-//                             136, 99, 108, 113, 127, 108, 113, 135, 101, 125,
-//                             109, 141, 109, 112, 142, 138, 93, 114, 121, 103,
-//                             116, 122, 125, 118, 127, 112, 120, 111, 106, 118,
-//                             119, 118, 121, 115, 107, 107, 94, 118, 104, 108,
-//                             117, 121, 125, 111, 113, 94, 112, 119, 130, 135,
-//                             120, 149, 111],
-//                         borderColor: '#c91b2b',
-//                         borderWidth: 1.5,
-//                         // hoverBackgroundColor: '#51FF0D',
-//                         // hoverBorderColor: 'azure',
-//                     },
-//                     {
-//                         label: 'Washingon Wizards Pts Per Game',
-//                         data: [112, 113, 125, 122, 112, 104, 95, 111, 108,
-//                             100, 108, 116, 117, 119, 104, 109, 125, 107, 124,
-//                             135, 104, 98, 102, 110, 131, 101, 101, 125, 118,
-//                             128, 110, 118, 149, 89, 95, 92, 130, 114, 109,
-//                             116, 115, 123, 113, 138, 101, 101, 118, 95, 119,
-//                             113, 107, 115, 129, 129, 119, 134, 112, 120, 110,
-//                             112, 125, 96, 135, 132, 111, 130, 121, 100, 110,
-//                             135, 95, 120, 108, 108, 106, 124, 124, 95, 114,
-//                             112, 110, 110,],
-//                         borderColor: '#007FFF ',
-//                         borderWidth: 1.5,
-//                         // hoverBackgroundColor: '#51FF0D',
-//                         // hoverBorderColor: 'azure',
-//                     }
-//                 ]
-//             }
-//         }
-//         )
-//     }
-
-
+    getStatsData() {
+        // Ajax call goes here, fill the state with the data that comes in
+        this.setState({
+            statsData: {
+                // The data below is hard coded. Need to change it so it dynamically pulls data depending on which teams the user selects
+                labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9',
+                    '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+                    '20', '22', '22', '23', '24', '25', '26', '27', '28', '29',
+                    '30', '33', '32', '33', '34', '35', '36', '37', '38', '39',
+                    '40', '44', '42', '43', '44', '45', '46', '47', '48', '49',
+                    '50', '55', '52', '53', '54', '55', '56', '57', '58', '59',
+                    '60', '66', '62', '63', '64', '65', '66', '67', '68', '69',
+                    '70', '77', '72', '73', '74', '75', '76', '77', '78', '79',
+                    '80', '81', '82'],
+                datasets: [
+                    {
+                        label: 'Houston Rockets Pts Per Game',
+                        data: [112, 124, 112, 89, 113, 85, 119, 96, 98,
+                            80, 89, 115, 109, 107, 132, 126, 111, 108, 131,
+                            108, 136, 121, 91, 91, 104, 111, 126, 105, 102,
+                            136, 99, 108, 113, 127, 108, 113, 135, 101, 125,
+                            109, 141, 109, 112, 142, 138, 93, 114, 121, 103,
+                            116, 122, 125, 118, 127, 112, 120, 111, 106, 118,
+                            119, 118, 121, 115, 107, 107, 94, 118, 104, 108,
+                            117, 121, 125, 111, 113, 94, 112, 119, 130, 135,
+                            120, 149, 111],
+                        borderColor: '#c91b2b',
+                        borderWidth: 1.5,
+                        // hoverBackgroundColor: '#51FF0D',
+                        // hoverBorderColor: 'azure',
+                    },
+                    {
+                        label: 'Washingon Wizards Pts Per Game',
+                        data: [112, 113, 125, 122, 112, 104, 95, 111, 108,
+                            100, 108, 116, 117, 119, 104, 109, 125, 107, 124,
+                            135, 104, 98, 102, 110, 131, 101, 101, 125, 118,
+                            128, 110, 118, 149, 89, 95, 92, 130, 114, 109,
+                            116, 115, 123, 113, 138, 101, 101, 118, 95, 119,
+                            113, 107, 115, 129, 129, 119, 134, 112, 120, 110,
+                            112, 125, 96, 135, 132, 111, 130, 121, 100, 110,
+                            135, 95, 120, 108, 108, 106, 124, 124, 95, 114,
+                            112, 110, 110,],
+                        borderColor: '#007FFF ',
+                        borderWidth: 1.5,
+                        // hoverBackgroundColor: '#51FF0D',
+                        // hoverBorderColor: 'azure',
+                    }
+                ]
+            }
+        }
+        )
+    }
 
 
-//     submitPlayers(e) {
-//         e.preventDefault();
-//         // console.log(this.state.player1, this.state.player2);
-//     }
 
-//     // submitTeams(e) {
-//     //     e.preventDefault();
-//     //     fetch("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/news")
-//     //         .then(res => res.json())
-//     //         .then(data => {
-//     //             this.setState({ data: data, submitted: true });
-//     //         });
-//     //     // console.log(this.state.team1, this.state.team2);
-//     // }
 
-//     updatePlayer1(e) {
-//         this.setState({ player1: e.target.value });
-//     }
+    submitPlayers(e) {
+        e.preventDefault();
+        // console.log(this.state.player1, this.state.player2);
+    }
 
-//     updatePlayer2(e) {
-//         this.setState({ player2: e.target.value });
-//     }
+    // submitTeams(e) {
+    //     e.preventDefault();
+    //     fetch("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/news")
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             this.setState({ data: data, submitted: true });
+    //         });
+    //     // console.log(this.state.team1, this.state.team2);
+    // }
 
-//     updateTeam1(e) {
-//         this.setState({ team1: e.target.value });
-//     }
+    updatePlayer1(e) {
+        this.setState({ player1: e.target.value });
+    }
 
-//     updateTeam2(e) {
-//         this.setState({ team2: e.target.value });
-//     }
+    updatePlayer2(e) {
+        this.setState({ player2: e.target.value });
+    }
+
+    updateTeam1(e) {
+        this.setState({ team1: e.target.value });
+    }
+
+    updateTeam2(e) {
+        this.setState({ team2: e.target.value });
+    }
 
     render() {
         return (
