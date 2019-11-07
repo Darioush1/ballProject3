119f9e063e1777bca902c4e8e51f045cd60fe3a4
Nary Hang <naryhang@gmail.com>
2019-11-06 22:21:14 -0800

    update utils api functions

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,25 +3,25 @@ import axios from "axios";
 export default {
     getPlayers: function (req,res) {
         //console.log("getPlayers called")
-        return axios.get("http://localhost:3001/api/player1/", )
+        return axios.get("/api/player1/", )
     },
  
     getPlayerA: function (req,res) {
         //console.log("getPlayers called")
-        return axios.get("http://localhost:3001/api/teamA/", )
+        return axios.get("/api/teamA/", )
     },
 
     savePlayer1: function (player1Stats) {
         
-        return axios.post("http://localhost:3001/api/teamA/", player1Stats)
+        return axios.post("/api/teamA/", player1Stats)
     },
 
     savePlayer2: function(player2Stats) {
-        return axios.post("http://localhost:3001/api/teamB/", player2Stats)
+        return axios.post("/api/teamB/", player2Stats)
     },
 
     clearTeam1: function (id) {
-        return axios.delete("http://localhost:3001/api/teamA/", id)
+        return axios.delete("/api/teamA/", id)
     }
 };
 
