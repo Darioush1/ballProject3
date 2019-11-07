5e2955b5bced1e799db5223346a16af31c7a7f3f
Nary Hang <naryhang@gmail.com>
2019-11-06 20:39:33 -0800

    update connection

--- api/mysql/connection.js
+++ api/mysql/connection.js
@@ -1,13 +1,19 @@
 const mysql = require("mysql");
 const express = require('express');
 const app = express();
-const connection = mysql.createConnection({
-    host     : process.env.JAWSDB_URL || 'localhost',
-    port    :3306,
-    user     : 'root',
-    password : 'bone1472',
-    database : 'nba_stats'
-});
+
+if(process.env.NODE_ENV === 'production') {
+    const connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host     : 'localhost',
+        port    :3306,
+        user     : 'root',
+        password : 'bone1472',
+        database : 'nba_stats'
+    });
+}
+
 
 
 connection.connect(function(err) {
